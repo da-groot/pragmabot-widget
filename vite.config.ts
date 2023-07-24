e8bd0fe7126bff2852b8c365c564c705c106ae67
da-groot <dagroot@protonmail.com>
2023-07-24 15:10:23 +0600

    add dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-import windiCSS from "vite-plugin-windicss";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-// });
 export default defineConfig({
-  plugins: [react(), windiCSS(), cssInjectedByJsPlugin()],
-  // base:
+  plugins: [react(), cssInjectedByJsPlugin()],
   build: {
     rollupOptions: {
       input: {
